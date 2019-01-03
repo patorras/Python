0912f247590e061ef1e762ace0d0798119097837
unknown <triotank@gmail.com>
2019-01-03 20:06:38 +0000

    new era

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 
+name =""
 
-print("What is your name?")
-name = input()
+while name != "your name":
+    print("Please type your name")
+    name = input()
 
-print("What is your age?")
-age = input()
-
-print("Nice to meet you " + name + " the year you will turn 100 is " + str(100 - int(age) + 2019))
+print("Thank you")
