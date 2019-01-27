146c1c675ad7d3528779516ce9e283cb34635652
patorras <triotank@gmail.com>
2019-01-27 11:28:13 +0000

    Update passGenerator.py

--- passGenerator.py
+++ passGenerator.py
@@ -2,9 +2,10 @@ import random, pyperclip, string
 
 
 com = random.randint(10,20)  #size of pass
-password = chr(random.randint(48,122)) #begining of pass
+password = string.ascii_letters[random.randint(0, 40)] + string.digits[random.randint(0,8)] #begining of pass
 
 for i in range(com):
   password = password + string.ascii_letters[random.randint(0, 40)] + string.digits[random.randint(0,8)]
 
+print(password)
 pyperclip.copy(password)
