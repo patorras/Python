592e84e0b734079f32be1ff95f990811f769e831
Pedro Lourenço <pedro.lourenco@driscolls.com>
2019-10-17 12:02:26 +0100

    new file

---
+++ edx50web/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO flights 
+    (origin, duration, destinatio)
+    VALUES ('New York', 'London', 415);
+
+INSERT INTO flights 
+    (origin, duration, destinatio)
+    VALUES ('Shangai', 'Paris', 760);
+
+INSERT INTO flights 
+    (origin, duration, destinatio)
+    VALUES ('Instanbul', 'Tokyo', 700);
+
+INSERT INTO flights 
+    (origin, duration, destinatio)
+    VALUES ('New York', 'Paris', 435);
+
+INSERT INTO flights 
+    (origin, duration, destinatio)
+    VALUES ('Moscow', 'Paris', 245);
+
+INSERT INTO flights 
+    (origin, duration, destinatio)
+    VALUES ('Lima', 'New York', 455);
